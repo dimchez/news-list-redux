3bc316453f5f245b2b248e577fa541a23c691f3c
Dmitry Demyankov <dmitry.demyankov@gmail.com>
2015-10-22 10:03:11 +0200

    updated devtools components

--- src/devtools.jsx
+++ src/devtools.jsx
@@ -4,33 +4,30 @@ import { Provider } from 'react-redux';
 import { devTools, persistState} from 'redux-devtools';
 import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 
-export function createStoreWithDevTools(middleware) {
-  return DEBUG ?
+export function createStoreWithDevTools(middleware, debug = DEBUG) {
+  return debug ?
     compose(middleware, devTools(), persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/)))(createStore) :
     middleware(createStore);
 }
 
-export default (Component, store) =>
+export const DebugPanelComponent = ({ store }) => {
+  return (
+    <DebugPanel top right bottom>
+      <DevTools store={ store } monitor={ LogMonitor } />
+    </DebugPanel>
+  );
+};
+
+export default (Component, store, debug = DEBUG) =>
   class extends React.Component {
-    createDebugPanel() {
-      if(!DEBUG) {
-        return null;
-      }
-
-      return (
-        <DebugPanel top right bottom>
-          <DevTools store={ store } monitor={ LogMonitor } />
-        </DebugPanel>
-      );
-    }
-
     render() {
+      const debugPanel = debug ? <DebugPanelComponent store={ store } /> : null;
       return (
         <div>
           <Provider store={ store }>
             <Component />
           </Provider>
-          { this.createDebugPanel() }
+          { debugPanel }
         </div>
       );
     }
